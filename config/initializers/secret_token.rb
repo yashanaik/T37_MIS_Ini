f9661c89bb253aef2ecdb83fa4370e7e0e060f69
Yasha Naik <yashanaik@gmail.com>
2013-07-16 13:45:15 -0400

    Project First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+T37MisIni::Application.config.secret_token = '410448106fbd4723931baa9bac70a5f285091bf178b8705b1e7095ce09004bff1c710d59c07fe79a462bc70cfffe2e5ce55f3bfc1fddd807f876662cf12028f8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+T37MisIni::Application.config.session_store :cookie_store, key: '_T37_MIS_Ini_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# T37MisIni::Application.config.session_store :active_record_store
